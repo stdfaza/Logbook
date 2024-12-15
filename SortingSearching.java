e6d5bb5732d0a495fa667d1b2ed520535dd789cb
stdfaza <myemail>
2024-12-15 21:32:22 +0800

    faza

--- SortingSearching.java
+++ SortingSearching.java
@@ -1,9 +1,32 @@
-class Node {
+class EventTree {
+    private TreeNode root;
 
-}
-class BinarySearchTree {
-    Node root;
-    void inorder() {
+    public void insert(Event event) {
+        root = insertRec(root, event);
+    }
 
+    private TreeNode insertRec(TreeNode root, Event event) {
+        if (root == null) {
+            root = new TreeNode(event);
+            return root;
+        }
+        if (event.getDate().compareTo(root.event.getDate()) < 0) {
+            root.left = insertRec(root.left, event);
+        } else {
+            root.right = insertRec(root.right, event);
+        }
+        return root;
+    }
+
+    public void inorderTraversal() {
+        inorderRec(root);
+    }
+
+    private void inorderRec(TreeNode root) {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.println(root.event);
+            inorderRec(root.right);
+        }
     }
 }
